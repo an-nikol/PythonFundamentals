171f17112af32497e90aa66c167e7e987393f653
an-nikol <142790207+an-nikol@users.noreply.github.com>
2023-09-14 16:51:53 +0300

    Create largest_of_three_numbers.py

---
+++ 01_lab/largest_of_three_numbers.py
@@ -0,0 +1,6 @@
+# print the largest number
+
+first_number, second_number, third_number = int(input()), int(input()), int(input())
+
+result = max(first_number, second_number, third_number)
+print(result)
