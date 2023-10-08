90fbb363187c0ef55c5f23da512fa8248e1ddccf
an-nikol <voynskaa@gmail.com>
2023-10-08 22:39:46 +0300

    updated 04_more_exercises.py

---
+++ 04_more_exercises/data_types.py
@@ -0,0 +1,15 @@
+def find_the_data_type(type, operation):
+    if type == 'int':
+        result = int(operation) * 2
+        print(result)
+    elif type == 'real':
+        result = int(operation) * 1.5
+        print(f'{result:.2f}')
+    elif type == 'string':
+        print(f'${operation}$')
+
+
+data_type = input()
+activity = input()
+
+find_the_data_type(data_type, activity)
