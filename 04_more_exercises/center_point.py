4f0b0e12514146b26b6796f8ac484835bb596919
an-nikol <voynskaa@gmail.com>
2023-10-10 18:49:21 +0300

    updated center_point

--- 04_more_exercises/center_point.py
+++ 04_more_exercises/center_point.py
@@ -1,5 +1,22 @@
-def center_point(x1,y1,x2,y2):
-    list_of_first_coordinates = [x1, y1]
-    list_of_second_coordinates = [x2, y2]
-    target_value = 0
-    closest_value = min()
+import math
+
+# create a tuple which items are formatted to the lower integer, which are floats and add each of them in a tuple
+points = tuple(math.floor(float(input())) for point in range(4))
+
+# find the abs sum of the two points
+
+# start with the first half of the tuple
+sum_x = sum([abs(num) for num in points[:2]])
+# continue with the second half
+sum_y = sum([abs(num) for num in points[2:]])
+
+
+# define a function to find the closes points
+def closer_set_of_points(first_point, second_point):
+    # the point that is closer to 0 shall be printed
+    if first_point <= second_point:
+        return points[:2]
+    else:
+        return points[2:]
+
+print(closer_set_of_points(sum_x, sum_y))
