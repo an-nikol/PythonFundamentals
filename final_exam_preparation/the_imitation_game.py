48f55c00e0652d26d0ca40012e3aca6cfd0a5a69
an-nikol <voynskaa@gmail.com>
2023-11-20 11:24:24 +0200

    added

---
+++ final_exam_preparation/the_imitation_game.py
@@ -0,0 +1,19 @@
+message = input()
+command = input().split("|")
+
+while "Decode" != command[0]:
+    if command[0] == "Move":
+        num = int(command[1])
+        message = message[num:] + message[:num]
+    elif command[0] == "Insert":
+        index = int(command[1])
+        value = str(command[2])
+        message = message[:index] + value + message[index:]
+    elif command[0] == "ChangeAll":
+        old = str(command[1])
+        new = str(command[2])
+        message = message.replace(old, new)
+
+    command = input().split("|")
+
+print(f"The decrypted message is: {message}")
